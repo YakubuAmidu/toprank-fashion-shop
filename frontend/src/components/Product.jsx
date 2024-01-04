d0b565ba4b61abda7722af943e78a725972407b4
YakubuAmidu <yakubu.amidu@yahoo.com>
2024-01-03 20:45:18 -0800

    Initial commit

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -4,6 +4,8 @@ import { Card } from "react-bootstrap";
 // React-router-dom
 import { Link } from "react-router-dom";
 
+import Rating from "./Rating";
+
 const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
@@ -13,10 +15,17 @@ const Product = ({ product }) => {
 
       <Card.Body>
         <Link to={`/product/${product._id}`}>
-          <Card.Title as="div">
+          <Card.Title as="div" className="product-title">
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
+
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews!`}
+          />
+        </Card.Text>
 
         <Card.Text as="h3">$ {product.price}</Card.Text>
       </Card.Body>
