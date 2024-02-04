e0e78c6cce1cadeda6012801d6432ff1c1a152c9
YakubuAmidu <yakubu.amidu@yahoo.com>
2024-02-04 07:57:08 -0800

    Initial commit

---
+++ backend/controllers/userController.js
@@ -0,0 +1,7 @@
+import asyncHandler from '../middleware/asyncHandler';
+import User from '../models/UserModel';
+
+// @desc Auth user & get token
+// @route  GET /api/users/login
+// @access public
+
