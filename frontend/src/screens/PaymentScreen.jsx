add1d12bdfdf1e0c64df88a9cd36aef53cdce228
YakubuAmidu <yakubu.amidu@yahoo.com>
2024-02-08 15:43:03 -0800

    Initial commit

--- frontend/src/screens/PaymentScreen.jsx
+++ frontend/src/screens/PaymentScreen.jsx
@@ -1,19 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { Form, Col, Button } from "react-bootstrap";
 import FormContainer from "../components/FormContainer";
 import CheckoutSteps from "../components/CheckOutSteps";
+import { savePaymentMethod } from "../slices/cartSlice";
 
 const PaymentScreen = () => {
   const [paymentMethod, setPaymentMethod] = useState("Paypal");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+  useEffect(() => {
+    if (!shippingAddress) {
+      navigate("/shipping");
+    }
+  }, [shippingAddress, navigate]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(savePaymentMethod(paymentMethod));
+    navigate("/placeorder");
+  };
 
   return (
     <FormContainer>
       <h1>Payment Method</h1>
       <CheckoutSteps step1 step2 />
 
-      <Form>
+      <Form onSubmit={submitHandler}>
         <Form.Group>
-          <Form.Label as="legent">Select Method</Form.Label>
+          <Form.Label as="legend">Select Method</Form.Label>
           <Col>
             <Form.Check
               type="radio"
