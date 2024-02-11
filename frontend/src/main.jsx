66ce9702675458539b15832c8ca06ca16ade785f
YakubuAmidu <yakubu.amidu@yahoo.com>
2024-02-11 09:12:25 -0800

    Initial commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -40,11 +40,11 @@ const router = createBrowserRouter(
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
 
-      <Route path="/" element={<PrivateRoute />}>
-      <Route path='/shipping' element={<ShippingScreen />} />
-        <Route path='/payment' element={<PaymentScreen />} />
-        <Route path='/placeorder' element={<PlaceOrderScreen />} />
-        <Route path='/order/:id' element={<OrderScreen />} />
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/shipping" element={<ShippingScreen />} />
+        <Route path="/payment" element={<PaymentScreen />} />
+        <Route path="/placeorder" element={<PlaceOrderScreen />} />
+        <Route path="/order/:id" element={<OrderScreen />} />
       </Route>
     </Route>
   )
