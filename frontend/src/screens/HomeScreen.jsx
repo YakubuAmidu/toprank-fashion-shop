0a86db1c6977a6794b1165dff9e6f5850afe39b1
YakubuAmidu <yakubu.amidu@yahoo.com>
2024-02-19 19:59:27 -0800

    Initial commit

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -5,12 +5,15 @@ import { Col, Row } from "react-bootstrap";
 // Imported-components
 // import products from "../products";
 import Product from "../components/Product";
+import { useParams } from "react-router-dom";
 import { useGetProductsQuery } from "../slices/productsApiSlice";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 
 const HomeScreen = () => {
-  const { data: products, isLoading, error } = useGetProductsQuery();
+  const { pageNumber } = useParams();
+  
+  const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
 
   return (
     <Fragment>
@@ -24,7 +27,7 @@ const HomeScreen = () => {
         <>
           <h1>Latest Products</h1>
           <Row>
-            {products.map((product) => (
+            {data.products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <Product product={product} />
               </Col>
